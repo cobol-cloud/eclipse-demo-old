c985bcfd64eb18661a14b0497e01c091d6b126f7
SezginH <Sezgin.Halibov@microfocus.com>
2018-12-05 15:28:17 +0200

    commit static web content produced by npm build

---
+++ StaticWebApp/WebContent/precache-manifest.2741d58a533e28acb2376820607d4f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4f590e39762c1e0227f5",
+    "url": "./static/js/main.4f590e39.chunk.js"
+  },
+  {
+    "revision": "54dbe162eb8f25e14a2d",
+    "url": "./static/js/1.54dbe162.chunk.js"
+  },
+  {
+    "revision": "4f590e39762c1e0227f5",
+    "url": "./static/css/main.73cf18e0.chunk.css"
+  },
+  {
+    "revision": "0aca54f5540bb3df90716d5962c93e47",
+    "url": "./index.html"
+  }
+];
